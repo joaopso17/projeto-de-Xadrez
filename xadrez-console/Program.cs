c749b0f61936ae92e1f5d49fdd884aee10a970f7
João Pedro <joaopedro17011@outlook.com>
2024-02-13 16:13:09 -0300

    melhorando o metado de imprimir o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,21 +1,32 @@
 ﻿using tabuleiro;
 using xadrez_console;
 using xadrez;
-using xadrez_console.xadrez;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-
-        PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-        Console.WriteLine(pos);
-
-
-        Console.WriteLine(pos.toPosicao());
+        try
+        {
+            Tabuleiro tab = new(8, 8);
 
 
+            tab.colocarPeca(new Torre(tab, Cor.preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.preta), new Posicao(2, 4));
+
+            tab.colocarPeca(new Torre(tab, Cor.branca), new Posicao(1, 1));
+            tab.colocarPeca(new Torre(tab, Cor.branca), new Posicao(1, 4));
+            tab.colocarPeca(new Rei(tab, Cor.branca), new Posicao(3, 3));
 
 
+
+            Tela.imprimirTabuleiro(tab);
+        }
+
+        catch (TabuleiroExeption ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
